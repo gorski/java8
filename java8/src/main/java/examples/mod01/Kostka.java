fee226c184a7898404cf11ff2cfe36fe3791cb0f
Marcin Górski <mg@mgorski.net>
2017-10-10 23:46:22 +0200

    Initial

---
+++ java8/src/main/java/examples/mod01/Kostka.java
@@ -0,0 +1,9 @@
+package examples.mod01;
+
+public class Kostka {
+	public static void main(String[] args) {
+		System.out.println("Program wyswietla liczbe oczek wyrzuconych kostka");
+
+		// losowanie liczby oczek 
+	}
+}
---
+++ java8/src/main/java/examples/mod01/Test.java
@@ -0,0 +1,8 @@
+package examples.mod01;
+
+public class Test {
+	public static void main(String args[]) {
+		Hello hello = new Hello();
+		hello.sayHello();
+	}
+}
